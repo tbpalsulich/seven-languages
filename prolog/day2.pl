5da67e65052f278fac768d64f63fc0408887b696
Tyler Palsulich <tpalsulich@gmail.com>
2013-07-17 11:37:18 -0700

    Prolog day 2 part 2.

--- prolog/day2.pl
+++ prolog/day2.pl
@@ -10,3 +10,10 @@ reverse([Head|Tail1], Return) :-
 	reverse(Tail1, Tail2),
 	append(Tail2, [Head], Return).
 
+minimum([A], A).
+minimum([Head|Tail], Return) :-
+	minimum(Tail, TailMin),
+	Head < TailMin, Return is Head.
+minimum([Head|Tail], Return) :-
+	minimum(Tail, TailMin),
+	Head >= TailMin, Return is TailMin.
