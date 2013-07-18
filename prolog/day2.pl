e552925436aa5f4dfaa4ee7d68f0fbd2f35bf57c
Tyler Palsulich <tpalsulich@gmail.com>
2013-07-17 17:37:55 -0700

    Adding comments to Prolog day 2.

--- prolog/day2.pl
+++ prolog/day2.pl
@@ -4,10 +4,10 @@
 
 % Reverse the elements of a list.
 
-reverse([A], [A]).
-reverse([A, B], [B, A]).
+reverse([A], [A]).					% Base case of just one element.
+reverse([A, B], [B, A]).			% Base case of two elements.
 
-reverse([Head|Tail1], Return) :-
+reverse([Head|Tail1], Return) :-	% Remove the Head, then append it to the reverse of the tail.
 	reverse(Tail1, Tail2),
 	append(Tail2, [Head], Return).
 
@@ -16,10 +16,10 @@ reverse([Head|Tail1], Return) :-
 
 % Find the smallest element of a list.
 
-minimum([A], A).
-minimum([Head|Tail], Head) :-
+minimum([A], A).					% Base case of just one element.
+minimum([Head|Tail], Head) :-		% Either return Head or the min of Tail.
 	minimum(Tail, TailMin),
-	Head < TailMin, !.
+	Head < TailMin, !.				% Cut to always return TailMin if this fails.
 minimum([_|Tail], TailMin) :-
 	minimum(Tail, TailMin).
 
@@ -29,16 +29,16 @@ minimum([_|Tail], TailMin) :-
 
 % Sort the elements of a list.
 
-merge(X, [], [X]).
-merge(X, [Y|Tail], [X,Y|Tail]) :-
-    X @< Y, !.
-merge(X, [Y|Tail2], [Y|Tail]) :-
-    merge(X, Tail2, Tail).
+% Merge will insert an element into the proper spot in the sorted list.
 
-msort([A], [A]).
-msort([A, B], [A, B]) :- A < B, !.
-msort([A, B], [B, A]).
-msort([Head|Tail], Result) :-
+merge(X, [], [X]).						% Base case of inserting into an empty list.
+merge(X, [Y|Tail], [X,Y|Tail]) :-		% Recursively take the Head off the list until the head is less than the element.
+    X @< Y, !.							% Cut to ensure we merge the element into the list at the right time.
+merge(X, [Y|Tail2], [Y|Tail]) :-
+    merge(X, Tail2, Tail).				% Once the element to insert is greater than the head, merge everything back.
+
+msort([A], [A]).						% Base case of the recursion. A one element list is already sorted.
+msort([Head|Tail], Result) :-			% Recursively merge the Head into the sorted Tail.
 	msort(Tail, SortedTail),
 	merge(Head, SortedTail, Result).
 
