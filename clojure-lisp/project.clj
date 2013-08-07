1a60b3753e16b76c428f027468f0896cd1a86696
Tyler Palsulich <tpalsulich@gmail.com>
2013-08-07 01:52:46 -0700

    Clojure day 2.

--- clojure-lisp/project.clj
+++ clojure-lisp/project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main clojure-lisp.day1)
+  :main clojure-lisp.day2)
---
+++ clojure-lisp/src/clojure_lisp/day2.clj
@@ -0,0 +1,35 @@
+; Tyler Palsulich
+; Seven Languages in Seven Weeks, by Bruce A. Tate
+; Clojure day 2
+
+(ns clojure-lisp.day2
+  (:gen-class))
+
+(defmacro unless [test body else-body]		; Problem 1. Simple expansion of the macro given.
+	(list 
+		'if (list 'not test)
+		body
+		else-body))
+
+(def numbers {"0" "nevereth", "1" "first", "2" "second"})	; Map to convert index to rank (for problem 2).
+
+(defprotocol Person ; Protocol for problem 2.
+	(introduce [p])	; Print the person's name.
+	(greet [p])		; Say hello.
+	(wave [p]))		; Wave and print how many times this person has waved.
+
+(defrecord SimplePerson [name waves]
+	Person
+	(introduce [_] (println (str "\"My name is " name ".\"")))
+	(greet [_] (println "\"Hello!\""))
+	(wave [_] (println (str name " waves at you for the " (numbers (str (+ waves 1))) " time.")) ; Use the map to convert the number of waves.
+		(SimplePerson. name (+ waves 1))))
+
+(defn -main []
+	(unless true (println "This should not print") (println "This should print"))
+	(unless false (println "This should print") (println "This should not print"))
+	(def tyler (SimplePerson. "Tyler" 0))
+	(def newTyler (wave tyler))		; Returns a new object, so we define a new reference.
+	(wave newTyler)
+	(greet newTyler)
+	(introduce newTyler))
